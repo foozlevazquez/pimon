1d006d59b8ee44732f4a93f3d45a192bc81382e4
Ivan Vazquez <ivan@ivanvazquez.com>
2015-12-16 22:19:26 -0700

    Fix temp get.

--- record_temps.sh
+++ record_temps.sh
@@ -4,6 +4,6 @@ cd /sys/bus/w1/devices
 
 for F in 28-*; do
     SN=`echo $F | sed 's/^28-...//' | sed 's/...$//'`
-    TEMP=`grep 't=' $SN | sed 's/^.*t=//' | awk '{ printf("%f\n", (($1 / 5000.0) * 9.0 ) + 32) }'`
+    TEMP=`grep 't=' ${F}/w1_slave | sed 's/^.*t=//' | awk '{ printf("%f\n", (($1 / 5000.0) * 9.0 ) + 32) }'`
     echo "temp.${SN}:${TEMP}|g" | nc -u -w0 alcazar 18125
 done
