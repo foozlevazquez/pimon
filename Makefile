d86b4248aec04a6c5df0685edae76c6c7e86b273
Ivan Vazquez <ivan@ivanvazquez.com>
2015-12-17 12:04:44 -0700

    Added name lookup.

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 #
 install:
-	sudo install -m 7555 record_temps.sh /usr/local/bin
+	touch / || echo "Must run as root" && exit
+	apt-get update && apt-get -y install dnsutils
+	install -m 7555 record_temps.sh /usr/local/bin
 	echo "* * * * * root /usr/local/bin/record_temps.sh" > /etc/cron.d/record_temps
 	grep -q w1-gpio /etc/modules || echo "w1-gpio" >> /etc/modules
 	grep -q w1-therm /etc/modules || echo "w1-therm" >> /etc/modules
--- record_temps.sh
+++ record_temps.sh
@@ -4,6 +4,7 @@ cd /sys/bus/w1/devices
 
 for F in 28-*; do
     SN=`echo $F | sed 's/^28-...//' | sed 's/...$//'`
+    NAME=`dig -t txt +noall +answer ${SN}.sensors.local | awk '{print $NF } | sed 's/"//g'`
     TEMP=`grep 't=' ${F}/w1_slave | sed 's/^.*t=//' | awk '{ printf("%f\n", (($1 / 5000.0) * 9.0 ) + 32) }'`
-    echo "temp.${SN}:${TEMP}|g" | nc -u -w0 alcazar 18125
+    echo "temp.${NAME}:${TEMP}|g" | nc -u -w0 alcazar 18125
 done
